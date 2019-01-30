ed6a827e328811b03bfb1e9a4e411ab1b6e281d9
Bharat Poptwani <bharat.poptwani@gmail.com>
2019-01-30 17:01:48 +0530

    unmarshalling json using mapstring interface

--- main.go
+++ main.go
@@ -5,30 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strconv"
 )
-
-// Users struct which contains
-// an array of users
-type Users struct {
-	Users []User `json:"users"`
-}
-
-// User struct which provides an
-// individual user's details
-type User struct {
-	Name   string `json:"name"`
-	Type   string `json:"type"`
-	Age    int    `json:"age"`
-	Social Social `json:"social"`
-}
-
-// Social struct which contains a
-// list of links
-type Social struct {
-	Facebook string `json:"facebook"`
-	Twitter  string `json:"twitter"`
-}
 
 func main() {
 	// open our jsonFile
@@ -44,15 +21,8 @@ func main() {
 	// read our opened jsonFile as a byte array.
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-	// we initialize our Users array
-	var users Users
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &result)
 
-	json.Unmarshal(byteValue, &users)
-
-	for i := 0; i < len(users.Users); i++ {
-		fmt.Println("User Type: " + users.Users[i].Type)
-		fmt.Println("User Age: " + strconv.Itoa(users.Users[i].Age))
-		fmt.Println("User Name: " + users.Users[i].Name)
-		fmt.Println("Facebook Url: " + users.Users[i].Social.Facebook)
-	}
+	fmt.Println(result["users"])
 }
